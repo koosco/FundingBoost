4d98d292a2caf1ec49fcd0e0633cf49e361dd9da
koosco <rnxogud123@naver.com>
2024-12-16 00:04:35 +0900

    initial project

---
+++ src/main/java/practice/fundingboost2/FundingBoost2Application.java
@@ -0,0 +1,13 @@
+package practice.fundingboost2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundingBoost2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundingBoost2Application.class, args);
+    }
+
+}
---
+++ src/test/java/practice/fundingboost2/FundingBoost2ApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.fundingboost2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundingBoost2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
