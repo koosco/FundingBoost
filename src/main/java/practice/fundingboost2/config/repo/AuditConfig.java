3206754875adda3cc484b98ffecc7f3e8277c174
koosco <rnxogud123@naver.com>
2024-12-16 01:04:59 +0900

    config: audit config

---
+++ src/main/java/practice/fundingboost2/config/repo/AuditConfig.java
@@ -0,0 +1,8 @@
+package practice.fundingboost2.config.repo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditConfig {}
