26ecd6ab33608486aac3df060bad0aa10f1e89a6
koosco <rnxogud123@naver.com>
2024-12-18 03:47:33 +0900

    fix: GifthubController path variable 적용
    restful api에 맞게 query parameter 대신 path variable로 변경

--- src/main/java/practice/fundingboost2/item/gifthub/ui/GifthubController.java
+++ src/main/java/practice/fundingboost2/item/gifthub/ui/GifthubController.java
@@ -2,8 +2,9 @@ package practice.fundingboost2.item.gifthub.ui;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import practice.fundingboost2.common.dto.CommonSuccessDto;
 import practice.fundingboost2.common.dto.ResponseDto;
 import practice.fundingboost2.config.security.annotation.Auth;
 import practice.fundingboost2.item.gifthub.app.GifthubService;
+import practice.fundingboost2.item.gifthub.app.dto.GifthubResponseDto;
 
 @RestController
 @RequestMapping("/api/gifthub")
@@ -19,22 +21,21 @@ public class GifthubController {
 
     private final GifthubService gifthubService;
 
-    @PostMapping
-    public ResponseDto<CommonSuccessDto> addItemToCart(@Auth Long memberId, @RequestParam Long itemId,
-        @RequestBody Long optionId) {
+    @PostMapping("/{item_id}/{option_id}")
+    public ResponseDto<CommonSuccessDto> addItemToCart(@Auth Long memberId, @PathVariable("item_id") Long itemId,
+        @PathVariable("option_id") Long optionId) {
         return ResponseDto.ok(gifthubService.addToCart(memberId, itemId, optionId));
     }
 
-    @DeleteMapping
-    public ResponseDto<CommonSuccessDto> deleteGifthub(@Auth Long memberId, @RequestParam Long itemId,
-        @RequestBody Long optionId) {
+    @DeleteMapping("{item_id}/{option_id}")
+    public ResponseDto<CommonSuccessDto> deleteGifthub(@Auth Long memberId, @PathVariable("item_id") Long itemId,
+        @PathVariable("option_id") Long optionId) {
         return ResponseDto.ok(gifthubService.deleteGifthub(memberId, itemId, optionId));
     }
-//
-//    @PatchMapping("/{item_id}")
-//    public ResponseDto<GifthubResponseDto> updateGifthub(@Auth Long memberId, @PathVariable("item_id") Long itemId,
-//        @RequestBody
-//        UpdateGifthubRequestDto dto) {
-//        return ResponseDto.ok(gifthubService.updateGifthub(memberId, itemId, dto));
-//    }
+
+    @PatchMapping("/{item_id}/{option_id}")
+    public ResponseDto<GifthubResponseDto> updateGifthub(@Auth Long memberId, @PathVariable("item_id") Long itemId,
+        @PathVariable("option_id") Long optionId, @RequestParam("quantity") Integer quantity) {
+        return ResponseDto.ok(gifthubService.updateGifthub(memberId, itemId, optionId, quantity));
+    }
 }
