0d7c259c6088bd3f024d91404cff8579b15926b9
koosco <rnxogud123@naver.com>
2024-12-18 04:26:43 +0900

    refactor: controller 메서드명 수정
    가독성을 위해 책임에 맞게 메서드명을 수정헀습니다

--- src/main/java/practice/fundingboost2/item/gifthub/ui/GifthubController.java
+++ src/main/java/practice/fundingboost2/item/gifthub/ui/GifthubController.java
@@ -2,6 +2,7 @@ package practice.fundingboost2.item.gifthub.ui;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import practice.fundingboost2.common.dto.CommonSuccessDto;
 import practice.fundingboost2.common.dto.ResponseDto;
 import practice.fundingboost2.config.security.annotation.Auth;
 import practice.fundingboost2.item.gifthub.app.GifthubService;
+import practice.fundingboost2.item.gifthub.ui.dto.GetGifthubResponseDto;
 
 @RestController
 @RequestMapping("/api/gifthub")
@@ -27,13 +29,13 @@ public class GifthubController {
     }
 
     @DeleteMapping("{item_id}/{option_id}")
-    public ResponseDto<CommonSuccessDto> deleteGifthub(@Auth Long memberId, @PathVariable("item_id") Long itemId,
+    public ResponseDto<CommonSuccessDto> deleteItemFromCart(@Auth Long memberId, @PathVariable("item_id") Long itemId,
         @PathVariable("option_id") Long optionId) {
         return ResponseDto.ok(gifthubService.deleteFromCart(memberId, itemId, optionId));
     }
 
     @PatchMapping("/{item_id}/{option_id}")
-    public ResponseDto<CommonSuccessDto> updateGifthub(@Auth Long memberId, @PathVariable("item_id") Long itemId,
+    public ResponseDto<CommonSuccessDto> updateItemFromCart(@Auth Long memberId, @PathVariable("item_id") Long itemId,
         @PathVariable("option_id") Long optionId, @RequestParam("quantity") Integer quantity) {
         return ResponseDto.ok(gifthubService.updateCart(memberId, itemId, optionId, quantity));
     }
