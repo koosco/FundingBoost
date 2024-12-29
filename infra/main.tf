d50bb9a2ef7cd5cd7959c7c3f5ea842d0eab903c
koosco <rnxogud123@naver.com>
2024-12-29 16:38:11 +0900

    infra: aws vpc 추가

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,7 @@
 provider "aws" {
-  region = "ap-northeas-2"
+  region = "ap-northeast-2"
+}
+
+module "vpc" {
+  source = "./modules/vpc"
 }
---
+++ infra/modules/vpc/outputs.tf
@@ -0,0 +1,7 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "route_table_id" {
+  value = aws_route_table.main.id
+}
