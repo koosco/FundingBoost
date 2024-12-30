5b090f1bf8ddfe0e1de4d86b984f814d8bf39168
koosco <rnxogud123@naver.com>
2024-12-31 01:43:47 +0900

    fix: gifthub 목록 조회 반환값 Page로 수정

--- src/main/java/practice/fundingboost2/item/gifthub/ui/GifthubQueryController.java
+++ src/main/java/practice/fundingboost2/item/gifthub/ui/GifthubQueryController.java
@@ -1,6 +1,7 @@
 package practice.fundingboost2.item.gifthub.ui;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import practice.fundingboost2.common.dto.ResponseDto;
 import practice.fundingboost2.config.security.annotation.Auth;
 import practice.fundingboost2.item.gifthub.repo.GifthubQueryRepository;
-import practice.fundingboost2.item.gifthub.ui.dto.GetGifthubListResponseDto;
+import practice.fundingboost2.item.gifthub.ui.dto.GetGifthubResponseDto;
 
 @RestController
 @RequestMapping("/api/gifthub")
@@ -18,7 +19,7 @@ public class GifthubQueryController {
     private final GifthubQueryRepository gifthubQueryRepository;
 
     @GetMapping
-    public ResponseDto<GetGifthubListResponseDto> getGifthub(@Auth Long memberId, Pageable pageable) {
+    public ResponseDto<Page<GetGifthubResponseDto>> getGifthub(@Auth Long memberId, Pageable pageable) {
         return ResponseDto.ok(gifthubQueryRepository.getAllGifthub(memberId, pageable));
     }
 }
