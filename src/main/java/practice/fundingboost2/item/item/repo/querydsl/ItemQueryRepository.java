f894e710e45d77a1c024c021acd4ad2e40396ecc
koosco <rnxogud123@naver.com>
2024-12-31 06:25:00 +0900

    fix: 찜한 목록 페이징 처리

--- src/main/java/practice/fundingboost2/item/item/repo/querydsl/ItemQueryRepository.java
+++ src/main/java/practice/fundingboost2/item/item/repo/querydsl/ItemQueryRepository.java
@@ -1,20 +1,15 @@
 package practice.fundingboost2.item.item.repo.querydsl;
 
-import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import practice.fundingboost2.item.item.ui.dto.GetItemDetailResponseDto;
-import practice.fundingboost2.item.item.ui.dto.GetItemListResponseDto;
-import practice.fundingboost2.item.item.repo.entity.Item;
 import practice.fundingboost2.item.item.ui.dto.GetItemResponseDto;
 
 public interface ItemQueryRepository {
 
     Page<GetItemResponseDto> getItems(String category, Pageable pageable);
 
-    GetItemListResponseDto getLikedItems(Long memberId, Pageable pageable);
-
-    Optional<Item> findItemByIdWithOptions(Long itemId);
+    Page<GetItemResponseDto> getLikedItems(Long memberId, Pageable pageable);
 
     GetItemDetailResponseDto getItemInfo(Long memberId, Long itemId);
 }
