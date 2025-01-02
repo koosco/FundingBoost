a84db1cd2f001a1b6d2f5be60b8e185537cbd6c5
koosco <rnxogud123@naver.com>
2025-01-03 07:51:49 +0900

    fix: dto convention 적용
    가독성을 고려해 dto의 code convention 일치

--- src/main/java/practice/fundingboost2/item/funding/app/dto/GetFundingHistoryResponseDto.java
+++ src/main/java/practice/fundingboost2/item/funding/app/dto/GetFundingHistoryResponseDto.java
@@ -4,10 +4,17 @@ import java.time.LocalDateTime;
 import practice.fundingboost2.item.funding.repo.entity.Funding;
 import practice.fundingboost2.item.funding.repo.entity.FundingTag;
 
-public record GetFundingHistoryResponseDto(Long fundingId, FundingTag fundingTag, LocalDateTime fundingStart, LocalDateTime fundingDeadline, int totalPrice, int collectPrice,
-                                           int contributorCount) {
-    public static GetFundingHistoryResponseDto from(Funding funding, int contributorCount){
+public record GetFundingHistoryResponseDto(
+    Long fundingId,
+    FundingTag fundingTag,
+    LocalDateTime fundingStart,
+    LocalDateTime fundingDeadline,
+    int totalPrice,
+    int collectPrice,
+    int contributorCount) {
+
+    public static GetFundingHistoryResponseDto from(Funding funding, int contributorCount) {
         return new GetFundingHistoryResponseDto(funding.getId(), funding.getTag(), funding.getCreatedAt(),
-                funding.getDeadLine(), funding.getTotalPrice(), funding.getCollectPrice(), contributorCount);
+            funding.getDeadLine(), funding.getTotalPrice(), funding.getCollectPrice(), contributorCount);
     }
 }
--- src/main/java/practice/fundingboost2/item/funding/app/dto/UpdateFundingRequest.java
+++ src/main/java/practice/fundingboost2/item/funding/app/dto/UpdateFundingRequest.java
@@ -2,7 +2,8 @@ package practice.fundingboost2.item.funding.app.dto;
 
 import java.time.LocalDateTime;
 
-public record UpdateFundingRequest(LocalDateTime deadline,
-                                    String fundingStatus) {
+public record UpdateFundingRequest(
+    LocalDateTime deadline,
+    String fundingStatus) {
 
 }
