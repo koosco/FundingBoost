0ba752d8e384ecac9b823a974e72910a64f79b86
koosco <rnxogud123@naver.com>
2025-01-05 12:25:27 +0900

    feat: MerchantUidGenerator 추가
    주문 번호를 위한 `날짜 + 임의 숫자`를 포함한 주문 번호 생성기

---
+++ src/main/java/practice/fundingboost2/item/order/app/MerchantUidGenerator.java
@@ -0,0 +1,17 @@
+package practice.fundingboost2.item.order.app;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ThreadLocalRandom;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MerchantUidGenerator {
+
+    public String generate() {
+        String prefix = LocalDate.now().format(DateTimeFormatter.ofPattern("yyMMdd"));
+        int randomSuffix = ThreadLocalRandom.current().nextInt(10000, 99999); // 10000 ~ 99999
+
+        return prefix + randomSuffix;
+    }
+}
