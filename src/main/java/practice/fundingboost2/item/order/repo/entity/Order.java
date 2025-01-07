b495c5abb0e0f5e36ff1e925907af2f276f55a05
koosco <rnxogud123@naver.com>
2025-01-08 05:40:23 +0900

    refactor: order 리뷰 검증 로직 추가
    * 사용자 검증
    * 이미 리뷰를 작성했는지 검증

--- src/main/java/practice/fundingboost2/item/order/repo/entity/Order.java
+++ src/main/java/practice/fundingboost2/item/order/repo/entity/Order.java
@@ -15,6 +15,8 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import practice.fundingboost2.common.exception.CommonException;
+import practice.fundingboost2.common.exception.ErrorCode;
 import practice.fundingboost2.common.repo.entity.BaseTimeEntity;
 import practice.fundingboost2.item.item.repo.entity.Item;
 import practice.fundingboost2.member.repo.entity.Member;
@@ -52,15 +54,40 @@ public class Order extends BaseTimeEntity {
     @Column(nullable = false)
     private OrderStatus orderStatus = OrderStatus.PENDING_PAYMENT;
 
+    @Column(nullable = false)
+    private boolean reviewWritten;
+
     public Order(Member member, Item item, String optionName, Delivery delivery, Integer quantity) {
         this.member = member;
         this.item = item;
         this.optionName = optionName;
         this.delivery = delivery;
         this.quantity = quantity;
+        this.reviewWritten = false;
     }
 
     public Order(Member member, Item item , String optionName, Delivery delivery) {
         this(member, item , optionName, delivery, DEFAULT_QUANTITY);
+    }
+
+    public void writeReview() {
+        reviewWritten = true;
+    }
+
+    public void validateReviewWritten(Long memberId) {
+        validateMember(memberId);
+        validateReviewWritten();
+    }
+
+    private void validateMember(Long memberId) {
+        if (member.validateId(memberId)) {
+            throw new CommonException(ErrorCode.NOT_FOUND_PURCHASED_ORDER);
+        }
+    }
+
+    private void validateReviewWritten() {
+        if (reviewWritten) {
+            throw new CommonException(ErrorCode.ALREADY_WRITTEN_REVIEW);
+        }
     }
 }
