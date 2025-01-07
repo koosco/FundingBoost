01473141ce62c41bc3435943632844899f66f6d7
koosco <rnxogud123@naver.com>
2025-01-08 06:21:45 +0900

    refactor: AuthController 검증 로직 추가

--- src/main/java/practice/fundingboost2/auth/ui/AuthController.java
+++ src/main/java/practice/fundingboost2/auth/ui/AuthController.java
@@ -2,14 +2,15 @@ package practice.fundingboost2.auth.ui;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import practice.fundingboost2.auth.app.AuthService;
 import practice.fundingboost2.auth.app.dto.LoginRequestDto;
-import practice.fundingboost2.auth.app.dto.LoginResponseDto;
 import practice.fundingboost2.auth.app.dto.RegisterRequestDto;
+import practice.fundingboost2.auth.app.dto.TokenDto;
 import practice.fundingboost2.common.dto.CommonSuccessDto;
 import practice.fundingboost2.common.dto.ResponseDto;
 
@@ -21,12 +22,18 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseDto<LoginResponseDto> login(@RequestBody LoginRequestDto dto) {
+    public ResponseDto<TokenDto> login(
+        @RequestBody
+        @Validated
+        LoginRequestDto dto) {
         return ResponseDto.ok(authService.login(dto));
     }
 
     @PostMapping("/register")
-    public ResponseDto<CommonSuccessDto> register(@RequestBody RegisterRequestDto dto) {
+    public ResponseDto<CommonSuccessDto> register(
+        @RequestBody
+        @Validated
+        RegisterRequestDto dto) {
         return ResponseDto.created(authService.register(dto));
     }
 }
