99e1e6de815716b0e008e1c83137d1e5eff1c180
koosco <rnxogud123@naver.com>
2025-01-08 15:19:27 +0900

    feat: review 삭제 api 추가

--- src/main/java/practice/fundingboost2/item/item/app/ReviewService.java
+++ src/main/java/practice/fundingboost2/item/item/app/ReviewService.java
@@ -5,6 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import practice.fundingboost2.common.dto.CommonSuccessDto;
+import practice.fundingboost2.common.exception.CommonException;
+import practice.fundingboost2.common.exception.ErrorCode;
 import practice.fundingboost2.common.file.FileService;
 import practice.fundingboost2.item.item.app.dto.CreateReviewRequestDto;
 import practice.fundingboost2.item.item.app.dto.GetReviewResponseDto;
@@ -26,6 +29,11 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final FileService fileService;
 
+    public Review findReview(Long reviewId) {
+        return reviewRepository.findById(reviewId)
+            .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_REVIEW));
+    }
+
     @Transactional
     public GetReviewResponseDto createReview(Long memberId, CreateReviewRequestDto dto, List<MultipartFile> files) {
         Member member = memberService.findMember(memberId);
@@ -41,5 +49,20 @@ public class ReviewService {
         return new GetReviewResponseDto(memberId, review.getId(),
             member.getNickname(), member.getImageUrl(), review.getScore(),
             review.getCreatedAt(), review.getContent(), review.getReviewImages());
+    }
+
+    @Transactional
+    public CommonSuccessDto deleteReview(Long memberId, Long reviewId) {
+        Review review = findReview(reviewId);
+        review.validateMember(memberId);
+        Item item = review.getItem();
+
+        item.removeReview();
+        List<String> reviewImageUrls = review.getReviewImages().stream()
+            .toList();
+        fileService.deleteFiles(reviewImageUrls);
+        reviewRepository.delete(review);
+
+        return CommonSuccessDto.fromEntity(true);
     }
 }
--- src/main/java/practice/fundingboost2/item/item/ui/ReviewController.java
+++ src/main/java/practice/fundingboost2/item/item/ui/ReviewController.java
@@ -3,11 +3,14 @@ package practice.fundingboost2.item.item.ui;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import practice.fundingboost2.common.dto.CommonSuccessDto;
 import practice.fundingboost2.common.dto.ResponseDto;
 import practice.fundingboost2.config.security.annotation.Auth;
 import practice.fundingboost2.item.item.app.ReviewService;
@@ -32,5 +35,15 @@ public class ReviewController {
 
         List<MultipartFile> files) {
         return ResponseDto.created(reviewService.createReview(memberId, dto, files));
+    }
+
+    @DeleteMapping("/{review_id}")
+    public ResponseDto<CommonSuccessDto> deleteReview(
+        @Auth
+        Long memberId,
+
+        @PathVariable("review_id")
+        Long reviewId) {
+        return ResponseDto.ok(reviewService.deleteReview(memberId, reviewId));
     }
 }
