fd456539759b4d5fb177041a6a9e5aecad42ca7a
koosco <rnxogud123@naver.com>
2025-01-09 15:03:47 +0900

    test: ContributorTest 추가

--- src/test/java/practice/fundingboost2/item/funding/app/ContributorServiceTest.java
+++ src/test/java/practice/fundingboost2/item/funding/app/ContributorServiceTest.java
@@ -1,54 +1,74 @@
 package practice.fundingboost2.item.funding.app;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
+import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import practice.fundingboost2.item.funding.app.interfaces.FundingRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import practice.fundingboost2.item.funding.repo.entity.Contributor;
-import practice.fundingboost2.item.funding.repo.entity.Funding;
 import practice.fundingboost2.item.funding.repo.jpa.ContributorRepository;
-import practice.fundingboost2.member.repo.MemberRepository;
-import practice.fundingboost2.member.repo.entity.Member;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class ContributorServiceTest {
 
-    @Autowired
+    @InjectMocks
     ContributorService contributorService;
 
-    @Autowired
+    @Mock
     ContributorRepository contributorRepository;
 
-    @Autowired
-    FundingRepository fundingRepository;
-
-    @Autowired
-    MemberRepository memberRepository;
+    @Mock
+    Contributor savedContributor;
 
     @Test
     void givenContributor_whenSave_thenCanFindContributor() {
         // given
-        Member member = new Member("email", "name", "image", "phone");
-        member.increasePoint(100_000);
-        Member friend = new Member("email", "name", "image", "phone");
-        friend.increasePoint(100_000);
-        Funding funding = new Funding(member, "message", "BIRTHDAY", LocalDateTime.now().plusDays(10));
-        funding.plusTotalPrice(10_000);
-        Contributor contributor = new Contributor(friend, funding, 1000);
-        memberRepository.save(member);
-        memberRepository.save(friend);
-        fundingRepository.save(funding);
-
+        Contributor savingContributor = mock(Contributor.class);
+        doReturn(savedContributor).when(contributorRepository).save(savingContributor);
         // when
-        Contributor savedContributor = contributorService.save(contributor);
+        contributorService.save(savingContributor);
 
         // then
-        Optional<Contributor> findContributor = contributorRepository.findById(savedContributor.getId());
-        assertThat(findContributor).isPresent();
-        assertThat(findContributor.get().getId()).isEqualTo(savedContributor.getId());
+        verify(contributorRepository, times(1)).save(savingContributor);
+    }
+
+    @Test
+    void givenContributors_whenFindAllByFundingId_thenReturnContributorList() {
+        // given
+        Long fundingId = 1L;
+        List<Contributor> contributors = List.of(
+            mock(Contributor.class),
+            mock(Contributor.class)
+        );
+        doReturn(contributors).when(contributorRepository).findAll_ByFundingId(fundingId);
+
+        // when
+        List<Contributor> result = contributorService.findAllByFundingId(fundingId);
+
+        // then
+        verify(contributorRepository, times(1)).findAll_ByFundingId(fundingId);
+        assertThat(result).hasSize(2);
+    }
+
+    @Test
+    void givenNoneContributors_whenFindAllByFundingId_thenReturnEmptyList() {
+        // given
+        Long fundingId = 1L;
+        List<Contributor> contributors = List.of();
+        doReturn(contributors).when(contributorRepository).findAll_ByFundingId(fundingId);
+
+        // when
+        List<Contributor> result = contributorService.findAllByFundingId(fundingId);
+
+        // then
+        verify(contributorRepository, times(1)).findAll_ByFundingId(fundingId);
+        assertThat(result).isEmpty();
     }
 }
