0d252ec76c12fe91ac40857f797d7b06072a26b7
koosco <rnxogud123@naver.com>
2025-01-09 09:45:01 +0900

    config: Swagger config 추가

---
+++ src/main/java/practice/fundingboost2/config/web/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package practice.fundingboost2.config.web;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(security = @SecurityRequirement(name = "bearerAuth"))
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        return new OpenAPI()
+            .components(new Components())
+            .info(apiInfo());
+
+    }
+
+    private Info apiInfo() {
+        return new Info()
+            .title("FundingBoost API")
+            .description("FundingBoost API 명세입니다.")
+            .version("1.0.0");
+    }
+}
+
